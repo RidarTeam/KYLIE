38c2c70cee69f9fe23278643fcd8e77e5822d673
KYLIE-TEAM <84586775+KYLIE-TEAM@users.noreply.github.com>
2021-07-07 22:23:45 +0300

    Add files via upload

--- KYLIE.lua
+++ KYLIE.lua
@@ -10094,7 +10094,7 @@ return false
 end
 if text == 'بوت' then
 Namebot = (database:get(bot_id..'Name:Bot') or 'كايلي')
-send(msg.chat_id_, msg.id_,'*اسمَـيہ '..Namebot..' *')
+send(msg.chat_id_, msg.id_,'اسمي الكيوت ['..Namebot..'] ')
 end
 if text == 'الاحصائيات' then
 if Sudo(msg) then 
