301e94d5112877a1aeec395203a97f7a37968a0d
KYLIE-TEAM <84586775+KYLIE-TEAM@users.noreply.github.com>
2021-07-15 03:29:48 +0300

    Add files via upload

--- KYLIE.lua
+++ KYLIE.lua
@@ -11208,45 +11208,6 @@ t = t..i.."-  "..v.." \n"
 end
 send(msg.chat_id_, msg.id_, t..'⋆ ┉  ┉  ┉  ┉ ┉  ┉  ┉  ┉ ⋆ٴ\n꙳.︙꙳. [𝙏𝙚𝘼𝙢 𝙆𝙮𝙡𝙞𝙚](t.me/ppppd) ')
 end
-if text == 'تعطيل الالعاب الاحترافيه' and Mod(msg) and msg.reply_to_message_id_ == 0 then 
-database:set(bot_id..'lock:geam'..msg.chat_id_,true)  
-send(msg.chat_id_, msg.id_,'*꙳.︙تم تعطيل الالعاب الاحترافيه*')
-end
-if text == 'تفعيل الالعاب الاحترافيه' and Mod(msg) and msg.reply_to_message_id_ == 0 then 
-database:del(bot_id..'lock:geam'..msg.chat_id_)  
-send(msg.chat_id_, msg.id_,'*꙳.︙تم تفعيل الالعاب الاحترافيه*')
-end
-if text == 'العاب كايلي' and Special(msg) then  
-if not database:get(bot_id..'lock:geam'..msg.chat_id_) then
-end
-local Teext =[[
-𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𝙩𝙤 𝙜𝙖𝙢𝙚𝙨 𝙆𝙮𝙡𝙞𝙚 .
-]]
-keyboard = {} 
-keyboard.inline_keyboard = {
-{{text="♟ Chess Game ♟",url='https://t.me/PZ6bot?game=chess'}},
-{{text="لعبة فلابي بيرد 🐥",url='https://t.me/awesomebot?game=FlappyBird'},{text="تحداني فالرياضيات 🔢",url='https://t.me/gamebot?game=MathBattle'}},
-{{text="تحداني في ❌⭕️",url='t.me/XO_AABOT?start3836619'},{text="سباق الدراجات 🏍",url='https://t.me/gamee?game=MotoFX'}},
-{{text="سباق سيارات 🏎",url='https://t.me/gamee?game=F1Racer'},{text="متشابه 👾",url='https://t.me/gamee?game=DiamondRows'}},
-{{text="كرة قدم ⚽",url='https://t.me/gamee?game=FootballStar'}},
-{{text="دومنا🥇",url='https://vipgames.com/play/?affiliateId=wpDom/#/games/domino/lobby'},{text="❕ليدو",url='https://vipgames.com/play/?affiliateId=wpVG#/games/ludo/lobby'}},
-{{text="ورق🤹‍♂",url='https://t.me/gamee?game=Hexonix'},{text="Hexonix❌",url='https://t.me/gamee?game=Hexonix'}},
-{{text="MotoFx🏍️",url='https://t.me/gamee?game=MotoFx'}},
-{{text="لعبة 2048 🎰",url='https://t.me/awesomebot?game=g2048'},{text="Squares🏁",url='https://t.me/gamee?game=Squares'}},
-{{text="Atomic 1▶️",url='https://t.me/gamee?game=AtomicDrop1'},{text="Corsairs",url='https://t.me/gamebot?game=Corsairs'}},
-{{text="LumberJack",url='https://t.me/gamebot?game=LumberJack'}},
-{{text="LittlePlane",url='https://t.me/gamee?game=LittlePlane'},{text="RollerDisco",url='https://t.me/gamee?game=RollerDisco'}},
-{{text="🦖 Dragon Game 🦖",url='https://t.me/PZ6bot?game=dragon'},{text="🐍 3D Snake Game 🐍",url='https://t.me/PZ6bot?game=snake'}},
-{{text="🔵 Color Game 🔴",url='https://t.me/PZ6bot?game=color'}},
-{{text="🚀 Rocket Game 🚀",url='https://t.me/PZ6bot?game=rocket'},{text="🏹 Arrow Game 🏹",url='https://t.me/PZ6bot?game=arrow'}},
-{{text = '𝙏𝙚𝘼𝙢 𝙆𝙮𝙡𝙞𝙚', url="t.me/PPPPD"}},
-{{text = 'مطور السورس', url="t.me/PPPPZ"}},
-}
-send_inlin_key(msg.chat_id_," *꙳.︙قائمه الالعاب الاحترافيه اضغط للعب*",_key,msg.id_)
-else
-send(msg.chat_id_, msg.id_," *꙳.︙الالعاب الاحترافيه معطله في الوقت الحالي *")
-end
-end
 if text == "تعطيل الابراج" and Manager(msg) then
 send(msg.chat_id_, msg.id_, '*꙳.︙تم تعطيل الابراج*')
 database:set(bot_id.." mode:brj_Bots"..msg.chat_id_,"close")
